ad3c92cac57590ecc504ea951e762c30078613a7
Steph1001 <Stephizunwa@gmail.com>
2022-05-25 03:03:07 +0100

    alx-low_level_programming/0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
- * print_list - prints all the elements  a list_t list.
- * @h: singly linked list.
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the list_t list to print.
+ *
  * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
