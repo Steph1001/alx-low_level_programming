ae121b243da38e0b17eaa5579656a7b08d83fe30
Steph1001 <Stephizunwa@gmail.com>
2022-06-02 01:09:37 +0100

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,46 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	int fd;
+	int amount, tot;
+	char *bufferc;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	bufferc = malloc(sizeof(char) * letters);
+	if (!bufferc)
+		return (0);
+
+	amount = read(fd, bufferc, letters);
+	if (amount < 0)
+	{
+		free(bufferc);
+		return (0);
+	}
+	bufferc[amount] = '\0';
+
+	close(fd);
+
+	tot = write(1, bufferc, amount);
+	if (tot < 0)
+	{
+		free(bufferc);
+		return (0);
+	}
+
+	free(bufferc);
+	return (tot);
 }
