781e649bb0f0731566c76b117b0a004fc7af9868
Steph1001 <Stephizunwa@gmail.com>
2022-06-02 01:22:35 +0100

    chages2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int amount, tot;
 	char *bufferc;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
